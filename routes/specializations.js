e0702077b622e8c72cbd413bb6818f4c42926d8e
oxenprogrammer <emmy1000okello@gmail.com>
2018-12-31 15:06:15 +0300

    ADD: implement specialization route

--- routes/specializations.js
+++ routes/specializations.js
@@ -5,8 +5,47 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const specializations = await specializations.find().sort('name');
+    const specializations = await Specialization.find().sort('name');
     res.send(specializations);
   });
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let specialization = new Specialization({name: req.body.name});
+  specialization = await specialization.save();
+
+  res.send(specialization);
+})
+
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const specialization = await Specialization.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+    new: true
+  });
+
+  if (!specialization) return res.status(404).send('The Specialization with the given ID was not found.');
+  
+  res.send(specialization);
+});
+
+router.delete('/:id', async (req, res) => {
+  const specialization = await Specialization.findByIdAndRemove(req.params.id);
+
+  if (!specialization) return res.status(404).send('The Specialization with the given ID was not found.');
+
+  res.send(specialization);
+});
+
+router.get('/:id', async (req, res) => {
+  const specialization = await Specialization.findById(req.params.id);
+
+  if (!specialization) return res.status(404).send('The Specialization with the given ID was not found.');
+
+  res.send(specialization);
+});
   
 module.exports = router;
