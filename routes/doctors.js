23232085e5c5f8de2611b5527db6e0dcaa2b596d
oxenprogrammer <emmy1000okello@gmail.com>
2019-02-25 12:51:17 +0300

    UPDATE: improve code with try catch block

--- routes/doctors.js
+++ routes/doctors.js
@@ -10,84 +10,107 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/me', authMiddleware, async (req, res) => {
-    const doctor = await Doctor.findById(req.user._id).select('-password');
-    res.send({'user': doctor}); 
+    try {
+        const doctor = await Doctor.findById(req.user._id).select('-password');
+        res.send({'user': doctor}); 
+    } catch (error) {
+        res.send({error});
+    }
 });
 
 router.get('/', authMiddleware, async (req, res) => {
-    const doctors = await Doctor.find().sort('firstName');
-    res.send(doctors);
+    try {
+        const doctors = await Doctor.find().sort('firstName');
+        res.send(doctors);
+    } catch (error) {
+        res.send({error})
+    }
 });
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
 
-    const specialization = await Specialization.findById(req.body.specializationId);
-    if (!specialization) return res.status(400).send({'message': 'Invalid Specialization.'});
+    try {
+        const specialization = await Specialization.findById(req.body.specializationId);
+        if (!specialization) return res.status(400).send({'message': 'Invalid Specialization.'});
 
-    let doctor = await Doctor.findOne({phone: req.body.phone});
-    if (doctor) return res.status(409).send({'message': `Doctor with phone number ${req.body.phone} already exist`});
+        let doctor = await Doctor.findOne({phone: req.body.phone});
+        if (doctor) return res.status(409).send({'message': `Doctor with phone number ${req.body.phone} already exist`});
+
+        doctor = new Doctor({ 
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            nin: req.body.nin,
+            dob: req.body.dob,
+            specialization: {
+                _id: specialization._id,
+                name: specialization.name
+            },
+            specializationId: req.body.specializationId,
+            postalAddress: req.body.postalAddress,
+            city: req.body.city,
+            phone: req.body.phone,
+            password: req.body.password,
+            priceRate: req.body.priceRate
+        });
+        const salt = await bcrypt.genSalt(10);
+        doctor.password = await bcrypt.hash(doctor.password, salt);
+        await doctor.save();
+        
+        res.send(_.pick(doctor,
+            ['_id', 'firstName', 'lastName', 'nin', 'dob',
+            'specialization', 'postalAddress', 'city', 'phone', 'priceRate' ]));
+    } catch (error) {
+        res.send({error});
+    }
   
-    doctor = new Doctor({ 
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      nin: req.body.nin,
-      dob: req.body.dob,
-      specialization: {
-          _id: specialization._id,
-          name: specialization.name
-      },
-      specializationId: req.body.specializationId,
-      postalAddress: req.body.postalAddress,
-      city: req.body.city,
-      phone: req.body.phone,
-      password: req.body.password,
-      priceRate: req.body.priceRate
-    });
-    const salt = await bcrypt.genSalt(10);
-    doctor.password = await bcrypt.hash(doctor.password, salt);
-    await doctor.save();
-    
-    res.send(_.pick(doctor,
-        ['_id', 'firstName', 'lastName', 'nin', 'dob',
-         'specialization', 'postalAddress', 'city', 'phone', 'priceRate' ]));
 });
 
 router.put('/:id', [authMiddleware, isActive], async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
-  
-    const doctor = await Doctor.findByIdAndUpdate(req.params.id,
-      { 
-        postalAddress: req.body.postalAddress,
-        city: req.body.city,
-        phone: req.body.phone,
-        password: req.body.password,
-        priceRate: req.body.priceRate,
-        isAvailable: req.body.isAvailable,
-        isActive: req.body.isActive
-      }, { new: true });
-  
-    if (!doctor) return res.status(404).send('The doctor with the given ID was not found.');
-    
-    res.send(doctor);
+
+    try {
+        const doctor = await Doctor.findByIdAndUpdate(req.params.id,
+            { 
+              postalAddress: req.body.postalAddress,
+              city: req.body.city,
+              phone: req.body.phone,
+              password: req.body.password,
+              priceRate: req.body.priceRate,
+              isAvailable: req.body.isAvailable,
+              isActive: req.body.isActive
+            }, { new: true }
+        );
+        
+        if (!doctor) return res.status(404).send('The doctor with the given ID was not found.');
+        res.send(doctor);
+    } catch (error) {
+        res.send({error});
+    }
 });
 
 router.delete('/:id', [authMiddleware, admin], async (req, res) => {
-    const doctor = await Doctor.findByIdAndRemove(req.params.id);
-  
-    if (!doctor) return res.status(404).send('The doctor with the given ID was not found.');
-  
-    res.send(doctor);
+
+    try {
+        const doctor = await Doctor.findByIdAndRemove(req.params.id);
+        if (!doctor) return res.status(404).send('The doctor with the given ID was not found.');
+        res.send(doctor);
+    } catch (error) {
+        res.send({error});
+    }
 });
 
 router.get('/:id', authMiddleware, async (req, res) => {
-const doctor = await Doctor.findById(req.params.id);
+    try {
+        const doctor = await Doctor.findById(req.params.id);
+        if (!doctor) return res.status(404).send('The doctor with the given ID was not found.');
+        res.send(doctor);
+    } catch (error) {
+        res.send({error});
+    }
 
-if (!doctor) return res.status(404).send('The doctor with the given ID was not found.');
-
-res.send(doctor);
 });
 
 module.exports = router;
--- routes/patients.js
+++ routes/patients.js
@@ -9,76 +9,97 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/me', authMiddleware, async (req, res) => {
-    const patient = await Patient.findById(req.user._id).select('-password');
-    res.send({'user': patient}); 
+    try {
+        const patient = await Patient.findById(req.user._id).select('-password');
+        res.send({'user': patient}); 
+    } catch (error) {
+        res.send({error});
+    }
 });
 
 router.get('/', [authMiddleware, isActive], async (req, res) => {
-    const pageSize = +req.query.pagesize;
-    const currentPage = +req.query.page;
-    const patients = await Patient.find().sort('name');
-    if (pageSize && currentPage) {
-        pagePatients = patients.skip(pageSize * (currentPage - 1))
-                .limit(pageSize);
-        res.send(pagePatients);    
-    }else {
-        res.send(patients);
-    }
+    try {
+        const pageSize = +req.query.pagesize;
+        const currentPage = +req.query.page;
+        const patients = await Patient.find().sort('name');
+        if (pageSize && currentPage) {
+            pagePatients = patients.skip(pageSize * (currentPage - 1))
+                    .limit(pageSize);
+            res.send(pagePatients);    
+        }else {
+            res.send(patients);
+        } 
+    } catch (error) {
+        res.send({error});
+    } 
 });
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
 
-    let patient = await Patient.findOne({phone: req.body.phone});
-    if (patient) return res.status(409).send('User with this phone number already exist');
+    try {
+        let patient = await Patient.findOne({phone: req.body.phone});
+        if (patient) return res.status(409).send('User with this phone number already exist');
 
-    patient = new Patient({ 
-      name: req.body.name,
-      phone: req.body.phone,
-      location: req.body.location,
-      dob: req.body.dob,
-      password: req.body.password
-    });
-    const salt = await bcrypt.genSalt(10);
-    patient.password = await bcrypt.hash(patient.password, salt);
-    await patient.save();
-    const token = patient.generateAuthToken();
-    res.header('x-auth-token', token).send(_.pick(patient, 
-        ['name', 'phone', 'location', 'dob']));
+        patient = new Patient({ 
+        name: req.body.name,
+        phone: req.body.phone,
+        location: req.body.location,
+        dob: req.body.dob,
+        password: req.body.password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        patient.password = await bcrypt.hash(patient.password, salt);
+        await patient.save();
+        const token = patient.generateAuthToken();
+        res.header('x-auth-token', token).send(_.pick(patient, 
+            ['name', 'phone', 'location', 'dob']));
+    } catch (error) {
+        res.send({error});
+    }
 });
 
 router.put('/:id', authMiddleware, async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
-  
-    const patient = await Patient.findByIdAndUpdate(req.params.id,
-      { 
-        name: req.body.name,
-        location: req.body.location,
-        phone: req.body.phone,
-        password: req.body.password
-      }, { new: true });
-  
-    if (!patient) return res.status(404).send('The patient with the given ID was not found.');
-    
-    res.send(patient);
+
+    try {
+        const patient = await Patient.findByIdAndUpdate(req.params.id,
+            { 
+              name: req.body.name,
+              location: req.body.location,
+              phone: req.body.phone,
+              password: req.body.password
+            }, { new: true });
+        
+          if (!patient) return res.status(404).send('The patient with the given ID was not found.');
+          
+          res.send(patient);
+    } catch (error) {
+        res.send({error});
+    }
 });
 
 router.delete('/:id', [authMiddleware, admin], async (req, res) => {
-    const patient = await Patient.findByIdAndRemove(req.params.id);
-  
-    if (!patient) return res.status(404).send('The patient with the given ID was not found.');
-  
-    res.send(patient);
+    try {
+        const patient = await Patient.findByIdAndRemove(req.params.id);
+        if (!patient) return res.status(404).send('The patient with the given ID was not found.');
+        res.send(patient);
+    } catch (error) {
+        res.send({error});
+    }
 });
 
 router.get('/:id', [authMiddleware, isActive], async (req, res) => {
-const patient = await Patient.findById(req.params.id);
-
-if (!patient) return res.status(404).send('The patient with the given ID was not found.');
-
-res.send(patient);
+    try {
+        const patient = await Patient.findById(req.params.id);
+        if (!patient) return res.status(404).send('The patient with the given ID was not found.');
+        res.send(patient);
+    } catch (error) {
+        res.send({error});
+    }
 });
 
 module.exports = router;
