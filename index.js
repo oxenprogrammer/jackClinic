c68fb3aa025c323ca2ab14f6065138238529b5e1
oxenprogrammer <emmy1000okello@gmail.com>
2019-02-26 11:01:15 +0300

    ADD: move route intialization

--- index.js
+++ index.js
@@ -4,37 +4,22 @@ require('winston-mongodb');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
-const specializations = require('./routes/specializations');
-const patients = require('./routes/patients');
-const doctors = require('./routes/doctors');
-const healthServices = require('./routes/healthServices');
 const config = require('config');
-const auth = require('./routes/auth');
-const login = require('./routes/login');
-const error = require('./middleware/error');
-const cors = require('cors');
 const express = require('express');
 const app = express();
-
-// process.on('uncaughtException', (error) => {
-//   console.log('SOMETHING WENT WRONG');
-//   winston.error(error.message, error);
-//   process.exit(1);
-// });
+require('./initialize/routes')(app);
 
 winston.handleExceptions(
   new winston.transports.File({filename: 'uncaughtExceptions.log'})
 );
 
 process.on('unhandledRejection', error => {
-  // winston.error(error.message, error);
-  // process.exit(1);
   throw error;
-})
+});
 
 winston.add(winston.transports.File, {filename: 'logfile.log'});
 winston.add(winston.transports.MongoDB, {db: 'mongodb://localhost/jackclinic', level: 'warn'});
-
+ 
 if (!config.get('authJWTPrivateKey')) {
   console.error('FATAL ERROR: authJWTPrivateKey not set');
   process.exit(1);
@@ -43,17 +28,6 @@ if (!config.get('authJWTPrivateKey')) {
 mongoose.connect('mongodb://localhost/jackclinic')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use(cors());
-app.use('/api/specializations', specializations);
-app.use('/api/doctors', doctors);
-app.use('/api/patients', patients);
-app.use('/api/healthservices', healthServices);
-app.use('/api/auth', auth);
-app.use('/api/login', login);
-
-app.use(error);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
