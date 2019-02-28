62d4aa8d89543848c78ec76d085ade5b7430ce31
oxenprogrammer <emmy1000okello@gmail.com>
2019-02-28 11:57:58 +0300

    UPDATE: export server

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ require('./initialize/db')();
 require('./initialize/validation')();
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+module.exports = server;
