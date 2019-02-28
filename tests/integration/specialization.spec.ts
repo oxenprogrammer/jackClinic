66182467a9c961afd9950ea03cc7695375eac80b
oxenprogrammer <emmy1000okello@gmail.com>
2019-02-28 14:32:58 +0300

    ADD: test get one specialization

--- tests/integration/specialization.spec.ts
+++ tests/integration/specialization.spec.ts
@@ -1,14 +1,41 @@
 let server;
 const request = require('supertest');
+const { Specialization }  = require('../../models/specialization');
 
 describe('/api/specializations', () => {
     beforeEach(() => { server = require('../../index') });
-    afterEach(() => { server.close() });
+    afterEach( async () => { 
+        server.close();
+        await Specialization.remove({})
+     }
+    );
 
-    describe('GET /', () => {
+    describe('GET /', async () => {
         it('should return all specializations',  async () => {
+            await Specialization.collection.insertMany(
+                [
+                    {name: 'Allergist'},
+                    {name: 'Nurse'},
+                    {name: 'Paramedic'}
+                ]
+            )
             const res = await request(server).get('/api/specializations');
-            expect(res.status).toBe(200)
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(3);
+            expect(res.body.some(s => s.name === 'Nurse')).toBeTruthy();
+            expect(res.body.some(s => s.name === 'Allergist')).toBeTruthy();
+        });
+    });
+
+    describe('GET /:id', async () => {
+        it('should return unique specialization if valid id is passed',  async () => {
+            const specialization = new Specialization({name: 'Allergist'});
+            await specialization.save();
+
+            const res = await request(server).get('/api/specializations/' + specialization._id);
+            expect(res.status).toBe(200);
+            // expect(res.body).toMatchObject(specialization);
+            expect(res.body).toHaveProperty('name', specialization.name);
         });
     });
 });
