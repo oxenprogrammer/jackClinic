53b3b05874262dd85b4b363155cf38894d9afe23
oxenprogrammer <emmy1000okello@gmail.com>
2019-02-28 11:38:45 +0300

    ADD: add test db

--- initialize/db.js
+++ initialize/db.js
@@ -1,10 +1,12 @@
 /*jshint esversion: 6 */
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = async () => {
+    const db = config.get('db');
     mongoose.set('useCreateIndex', true);
-    await mongoose.connect('mongodb://localhost/jackclinic', { useNewUrlParser: true });
-    winston.info('Connected to MongoDB . . .');
+    await mongoose.connect(db, { useNewUrlParser: true });
+    winston.info(`Connected to ${db} . . .`);
 }
 
